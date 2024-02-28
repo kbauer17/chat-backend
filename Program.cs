50a984b9a6bff7793ddd181603a1cf5881e0ede8
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 08:34:23 -0600

    [1.04] Update Program class - Enable CORS - Define CORS policy for SignalR - Add SignalR - Create endpoint for ChatHub

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Chat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// add the CORS service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Hubs",
+        builder =>
+        {
+            builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                // Anonymous origins NOT allowed for web sockets
+                .WithOrigins("https://localhost:7196","https://kbauer17.github.io")
+                .AllowCredentials();
+        });
+});
+
+// add the service for SignalR
+builder.Services.AddSignalR();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +38,18 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// add routing and designate the Hub with Cors
+app.UseRouting();
+app.UseCors("Hubs");
+
+
 app.UseAuthorization();
 
-app.MapControllers();
+// replace: app.MapControllers(); with the following mappings:
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/hubs/chat");
+});
 
 app.Run();
